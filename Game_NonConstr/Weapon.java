8207f935154f5689e75d19f2787aa8a357602d1d
Richard Jefferson Meneses <111546377+Richard091103@users.noreply.github.com>
2022-08-30 09:42:37 +0800

    Create Weapon.java

---
+++ Game_NonConstr/Weapon.java
@@ -0,0 +1,15 @@
+public class Weapon {
+
+    // Poperties/Attributes
+    String name;
+    int damage;
+    String rarity;
+    float weight;
+    String weaponDescription;
+
+    // Method
+    public void displayweapondescription() {
+        System.out.println("Weapon Description: " + weaponDescription);
+    }
+
+}
